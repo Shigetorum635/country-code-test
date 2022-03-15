e2ce691fad062590c41c08394835b0d529179712
Shiggy <ilarumbgim1@educacion.navarra.es>
2022-03-15 16:44:38 +0100

    feat: add cool json

--- code.js
+++ code.js
@@ -3,15 +3,23 @@ const express = require('express');
 const app = express();
 
 app.use(requestCountry.middleware({
-  attributeName: 'requestCountryCode', // default is 'requestCountryCode'
-  privateIpCountry: 'US' // Result for private network IPs
+    attributeName: 'requestCountryCode', // default is 'requestCountryCode'
+    privateIpCountry: 'US' // Result for private network IPs
 }));
 
 app.get('/', (req, res) => {
-    console.log(`AHH SEX`)
-	res.send(req.requestCountryCode);
+    console.log(`AHHHH SEX! (ok)`)
+    /**
+     * its the country.
+     * 
+     * that's it. it's just the country
+     * 
+     * What you looking at huh
+     */
+    const country = req.requestCountryCode
+    return res.status(200).json({code: country !== false ? country : "None", found: country === false ? false : true})
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env, () => {
     console.log(`Hosting on port 80...`)
 });
