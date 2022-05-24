3a46c5e4992500b2c3d8129f0537b569ac3a000c
Shiggy <48862659+Shigetorum635@users.noreply.github.com>
2022-05-24 10:36:10 +0000

    refactor: make it slightly more readable

--- code.js
+++ code.js
@@ -1,25 +1,30 @@
-const requestCountry = require('request-country');
-const express = require('express');
+const requestCountry = require("request-country");
+const express = require("express");
 const app = express();
 
-app.use(requestCountry.middleware({
-    attributeName: 'requestCountryCode', // default is 'requestCountryCode'
-    privateIpCountry: 'US' // Result for private network IPs
-}));
+app.use(
+  requestCountry.middleware({
+    attributeName: "requestCountryCode", // default is 'requestCountryCode'
+    privateIpCountry: "US", // Result for private network IPs
+  })
+);
 
-app.get('/', (req, res) => {
-    console.log(`AHHHH SEX! (ok)`)
-    /**
-     * its the country.
-     * 
-     * that's it. it's just the country
-     * 
-     * What you looking at huh
-     */
-    const country = req.requestCountryCode
-    return res.status(200).json({code: country !== false ? country : "None", found: country === false ? false : true})
-})
+app.get("/", (req, res) => {
+  /**
+   * Yup, that's the country.
+   * Very surprising
+   * wow!
+   */
+  const country = req.requestCountryCode;
+  if (!country) return res.status(404).json({ message: "Not found" });
+  else
+    return res.status(200).json({
+      code: country,
+    });
+});
 
-app.listen(process.env.PORT || 3000, () => {
-    console.log(`Hosting on port 80...`)
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Hosting on port ${port}!`);
 });
